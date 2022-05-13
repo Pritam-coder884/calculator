08dec1542b1cf000ea093947d04dcbeb303c4b66
Pritam-coder887 <pritamkumar72002@gmail.com>
2022-05-14 04:38:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+function getHistory(){
+    return document.getElementById("history-value").innerHTML;
+}
+function printHistory(num){
+    document.getElementById("history-value").innerHTML=num;
+}
+function getOutput(){
+    return document.getElementById("output-value").innerHTML;
+}
+function printOutput(num){
+    if(num==" "){
+        document.getElementById("output-value").innerHTML=num;
+    }
+    else{
+        document.getElementById("output-value").innerHTML=getformattedNumber(num);
+    }
+    document.getElementById("output-value").innerHTML=num;
+}
+function getformattedNumber(num){
+    var n=Number(num);
+    var value=n.toLocaleString("en");
+    return value;
+}
+function reverseNumberFormat(num){
+    return Number(num.replace(/,/g,''));
+}
+var operator=document.getElementsByClassName("operator");
+for(var i=0;i<operator.length;i++){
+    operator[i].addEventListener('click',function(){
+        if(this.id=="clear"){
+            printHistory("");
+            printOutput("");
+        }
+        else if(this.id=="backspace"){
+            var output=reverseNumberFormat(getOutput()).toString();
+            if(output){
+                output=output.substr(0,output.length-1);
+                printOutput(output);
+            }
+        }
+        else{
+            var output=getOutput();
+            var history=getHistory();
+            if(output!=""){
+                output=reverseNumberFormat(output);
+                history=history+output;
+                if(this.id=="="){
+                    var result=eval(history);
+                    printOutput(result);
+                    printHistory("");
+                }
+                else{
+                    history=history+this.id;
+                    printHistory(history);
+                    printOutput("");
+                }
+            }
+        }
+    });
+}
+var number=document.getElementsByClassName("number");
+for(var i=0;i<number.length;i++){
+    number[i].addEventListener('click',function(){
+        var output=reverseNumberFormat(getOutput());
+        if(output!=NaN){
+            output=output+this.id;
+            printOutput(output);
+        }
+    })
+}
+
+var point=document.getElementById("point");
+point.addEventListener('click',()=>{
+    document.getElementById("output-value").innerHTML=".";
+})
